cfe6dc38bb0f0e0a05bedb056989c5f587238aa9
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-22 10:23:55 +0100

    Refactor file sorting logic and update data files for improved functionality

--- FileTidy.CLI/Program.cs
+++ FileTidy.CLI/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FileTidy.CLI.Reporting;
+using FileTidy.Core.Services;
+using FileTidy.Services;
+
+List<string> directoriesToSort = new List<string>();
+
+while (true)
+{
+    Console.Write("Enter folder path(s) (comma-separated) or type 'exit' to quit: ");
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input)) continue;
+
+    if (input.Trim().ToLower() == "exit") break;
+
+    var paths = input.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(p => p.Trim())
+                        .ToList();
+
+    foreach (var path in paths)
+    {
+        string fullPath = DirectoryHelper.GetFullPath(path);
+
+        if (DirectoryHelper.CheckIfDirectoryExists(fullPath))
+        {
+            directoriesToSort.Add(fullPath);
+        }
+    }
+
+    foreach (var directoryToSort in directoriesToSort)
+    {
+        var reporter = new ConsoleSortReporter();
+
+        IEnumerable<string> allFiles = Directory.EnumerateFiles(directoryToSort, "*", SearchOption.AllDirectories);
+        reporter.SetTotalFiles(allFiles.Count());
+
+        string baseDirectory = AppContext.BaseDirectory;
+        string projectRoot = Directory.GetParent(baseDirectory)!.Parent!.Parent!.FullName;
+        string dataDirectory = Path.Combine(projectRoot, "FileTidy.CLI");
+
+        var mapper = new FileCategoryMapper(dataDirectory);
+        FileSorter sorter = new FileSorter(directoryToSort, mapper, reporter);
+        sorter.Sort();
+    }
+
+    directoriesToSort.Clear();
+}
