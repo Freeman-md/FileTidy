2cf2f1a6feba19825d9b25b593561e74d3325be5
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-24 12:06:14 +0100

    Extract styles and resources into separate files for modularity

--- FileTidy.GUI/App.axaml.cs
+++ FileTidy.GUI/App.axaml.cs
@@ -1,28 +1,28 @@
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Markup.Xaml;
-using FileTidy.GUI.ViewModels;
-using MainViewModel = FileTidy.GUI.ViewModels.MainViewModel;
+    using Avalonia;
+    using Avalonia.Controls.ApplicationLifetimes;
+    using Avalonia.Markup.Xaml;
+    using FileTidy.GUI.ViewModels;
+    using MainViewModel = FileTidy.GUI.ViewModels.MainViewModel;
 
-namespace FileTidy.GUI;
+    namespace FileTidy.GUI;
 
-public partial class App : Application
-{
-    public override void Initialize()
+    public partial class App : Application
     {
-        AvaloniaXamlLoader.Load(this);
-    }
-
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        public override void Initialize()
         {
-            desktop.MainWindow = new Views.MainView
-            {
-                DataContext = new MainViewModel()
-            };
+            AvaloniaXamlLoader.Load(this);
         }
 
-        base.OnFrameworkInitializationCompleted();
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new Views.MainView
+                {
+                    DataContext = new MainViewModel()
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
     }
-}
