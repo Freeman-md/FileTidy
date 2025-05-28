ab21e04631668c5c7aafdedff7716248b872d0cd
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-28 14:53:04 +0100

    feat(ui): render CurrentFiles with status badge component in file table

--- FileTidy.GUI/Components/StatusBadge.axaml.cs
+++ FileTidy.GUI/Components/StatusBadge.axaml.cs
@@ -1,23 +1,42 @@
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace FileTidy.GUI.Components;
 
-public partial class StatusBadge : UserControl
+public partial class StatusBadge : UserControl, INotifyPropertyChanged
 {
 
     public static readonly StyledProperty<string> StatusProperty =
         AvaloniaProperty.Register<StatusBadge, string>(nameof(Status), defaultValue: "Moved");
 
+    public IBrush BadgeColor => Status switch
+    {
+        "Moved" => Brushes.MediumSeaGreen,
+        "Skipped" => Brushes.Goldenrod,
+        "Pending" => Brushes.Gray,
+        _ => Brushes.LightGray,
+    };
+
     public string Status
     {
         get => GetValue(StatusProperty);
-        set => SetValue(StatusProperty, value);
+        set
+        {
+            SetValue(StatusProperty, value);
+            OnPropertyChanged(nameof(BadgeColor));
+        }
     }
-    
+
     public StatusBadge()
     {
         InitializeComponent();
     }
+    
+    public new event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
