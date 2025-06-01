812f2a1aa4a77574c7c0d043e326db2392b48c58
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-06-01 07:14:24 +0100

    fix: mark OriginalPath and NewPath as required in FileMoveResult

--- FileTidy.Core/Models/FileMoveResult.cs
+++ FileTidy.Core/Models/FileMoveResult.cs
@@ -2,8 +2,8 @@ namespace FileTidy.Core.Models;
 
 public class FileMoveResult
 {
-    public string OriginalPath { get; init; }
-    public string NewPath { get; init; }
+    public required string OriginalPath { get; init; }
+    public required string NewPath { get; init; }
     public FileOperationStatus Status { get; init; }
     public bool Success => Status == FileOperationStatus.Moved;
     public Exception? Error { get; init; }
