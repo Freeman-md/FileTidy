c2f3f9caeed63b2c689b72101963550a589edff4
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-08-15 06:35:10 +0100

    chore: remove SelectedFolders key from AppConfigKeys to streamline configuration

--- FileTidy.Core/Models/AppConfigKeys.cs
+++ FileTidy.Core/Models/AppConfigKeys.cs
@@ -5,6 +5,5 @@ public static class AppConfigKeys
     public const string HasCompletedOnboarding = "HasCompletedOnboarding";
     public const string LastSortSessionId = "LastSortSessionId";
     public const string DarkModeEnabled = "DarkModeEnabled";
-    public const string SelectedFolders = "SelectedFolders";
     public const string DeviceId = "DeviceId";
 }
